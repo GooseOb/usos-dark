ecb2b0fb8e7e16df2bd4c1e19defd53d592e98b3
GooseOb <gooose.ob@gmail.com>
2024-06-01 18:32:14 +0200

    optimize css & format code

--- bun-config.ts
+++ bun-config.ts
@@ -3,8 +3,8 @@ import { readFile } from "node:fs/promises";
 const define: Record<string, string> = {};
 
 const styleDeclarations = (await readFile("index.ts", "utf8"))
-  .match(/declare const(.+?);/s)[0]
-  .matchAll(/(s[A-Z_]+)\s*:\s*'(\S+\.css)'/g);
+  .match(/declare const(.+?);/s)![0]
+  .matchAll(/(s[A-Z_]+)\s*:\s*"(\S+\.css)"/g);
 
 for (const [, varName, fileName] of styleDeclarations)
   define[varName] =
--- index.ts
+++ index.ts
@@ -1,52 +1,54 @@
-declare const
-	sGLOBAL: 'global.css',
-	sSELECTOR: 'selector.css',
-	sCOPYRIGHT: 'copyright.css',
-	sHAMBURGER: 'hamburger.css',
-	sTIMETABLE_DAY: 'timetable-day.css',
-	sTIMETABLE: 'timetable.css';
+declare const sGLOBAL: "global.css",
+  sSELECTOR: "selector.css",
+  sCOPYRIGHT: "copyright.css",
+  sHAMBURGER: "hamburger.css",
+  sTIMETABLE_DAY: "timetable-day.css",
+  sTIMETABLE: "timetable.css";
 
 const applyStyles = (el: DocumentOrShadowRoot, cssText: string) => {
-	if (!el) return;
-	const styles = new CSSStyleSheet;
-	styles.replace(cssText).then(() => {
-		el.adoptedStyleSheets.push(styles);
-	});
+  if (!el) return;
+  const styles = new CSSStyleSheet();
+  styles.replace(cssText).then(() => {
+    el.adoptedStyleSheets.push(styles);
+  });
 };
 const applyStylesForEach = (selector: string, styles: string) => {
-	for (const el of document.querySelectorAll(selector))
-		applyStyles(el.shadowRoot, styles);
+  for (const el of document.querySelectorAll(selector))
+    applyStyles(el.shadowRoot, styles);
 };
-const getShadowRoot = (selector: string, parent: ParentNode = document) => parent.querySelector(selector)?.shadowRoot;
+const getShadowRoot = (selector: string, parent: ParentNode = document) =>
+  parent.querySelector(selector)?.shadowRoot;
 
 applyStyles(document, sGLOBAL);
 
 applyStyles(
-	getShadowRoot('menu-top-hamburger', getShadowRoot('menu-top')),
-	sHAMBURGER
+  getShadowRoot("menu-top-hamburger", getShadowRoot("menu-top")),
+  sHAMBURGER,
 );
 
-applyStyles(getShadowRoot('usos-copyright'), sCOPYRIGHT);
+applyStyles(getShadowRoot("usos-copyright"), sCOPYRIGHT);
 
-applyStylesForEach('usos-selector', sSELECTOR);
+applyStylesForEach("usos-selector", sSELECTOR);
 
-applyStyles(getShadowRoot('usos-timetable'), sTIMETABLE);
-applyStylesForEach('timetable-day', sTIMETABLE_DAY);
+applyStyles(getShadowRoot("usos-timetable"), sTIMETABLE);
+applyStylesForEach("timetable-day", sTIMETABLE_DAY);
 
 type RgbString = `rgb(${number}, ${number}, ${number})`;
 
 const tableColors = {
-	'rgb(236, 236, 236)': '#555',
-	'rgb(216, 216, 216)': '#444',
-	'rgb(238, 238, 221)': '#444',
-	'rgb(222, 222, 205)': '#333',
-	'rgb(156, 164, 152)': '#222',
-	'rgb(255, 255, 255)': '#333',
+  "rgb(236, 236, 236)": "#555",
+  "rgb(216, 216, 216)": "#444",
+  "rgb(238, 238, 221)": "#444",
+  "rgb(222, 222, 205)": "#333",
+  "rgb(156, 164, 152)": "#222",
+  "rgb(255, 255, 255)": "#333",
 } satisfies Record<RgbString, string>;
 type TableColor = keyof typeof tableColors;
 
 setTimeout(() => {
-	for (const td of document.querySelectorAll<HTMLTableCellElement>('tbody > tr > td, tbody > tr > th'))
-		td.style.backgroundColor =
-			tableColors[td.style.backgroundColor as TableColor] || '#000';
+  for (const td of document.querySelectorAll<HTMLTableCellElement>(
+    "tbody > tr > td, tbody > tr > th",
+  ))
+    td.style.backgroundColor =
+      tableColors[td.style.backgroundColor as TableColor] || "#000";
 }, 100);
