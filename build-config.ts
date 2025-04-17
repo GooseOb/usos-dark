9f49e42a49b0ae1fbcc986a09068200678662072
GooseOb <gooose.ob@gmail.com>
2025-04-18 00:20:48 +0200

    improve textfields style

--- build-config.ts
+++ build-config.ts
@@ -2,20 +2,20 @@ import { readFile } from "node:fs/promises";
 import type { BuildConfigs } from "bun-build-userscript";
 
 export const bun: BuildConfigs["bun"] = {
-  naming: "dist/index.js",
+	naming: "dist/index.js",
 };
 
 export const userscript: BuildConfigs["userscript"] = {
-  entry: "src",
-  before: async ({ bun }) => {
-    bun.define = {};
+	entry: "src",
+	before: async ({ bun }) => {
+		bun.define = {};
 
-    const styleDeclarations = (await readFile("src/index.ts", "utf8"))
-      .match(/declare const(.+?);/s)![0]
-      .matchAll(/(s[A-Z_]+)\s*:\s*"(\S+\.css)"/g);
+		const styleDeclarations = (await readFile("src/index.ts", "utf8"))
+			.match(/declare const(.+?);/s)![0]
+			.matchAll(/(s[A-Z_]+)\s*:\s*"(\S+\.css)"/g);
 
-    for (const [, varName, fileName] of styleDeclarations)
-      bun.define[varName] =
-        `"${(await readFile("src/styles/" + fileName, "utf8")).replace(/\s+/gs, " ")}"`;
-  },
+		for (const [, varName, fileName] of styleDeclarations)
+			bun.define[varName] =
+				`"${(await readFile("src/styles/" + fileName, "utf8")).replace(/\s+/gs, " ")}"`;
+	},
 };
--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,51 @@
 declare const sGLOBAL: "global.css",
-  sSELECTOR: "selector.css",
-  sCOPYRIGHT: "copyright.css",
-  sHAMBURGER: "hamburger.css",
-  sTIMETABLE_DAY: "timetable-day.css",
-  sTIMETABLE: "timetable.css";
+	sSELECTOR: "selector.css",
+	sCOPYRIGHT: "copyright.css",
+	sHAMBURGER: "hamburger.css",
+	sTIMETABLE_DAY: "timetable-day.css",
+	sTIMETABLE: "timetable.css",
+	sTEXT_FIELD: "text-field.css";
 
 const applyStyles = (el: DocumentOrShadowRoot, cssText: string) => {
-  if (!el) return;
-  const styles = new CSSStyleSheet();
-  styles.replace(cssText).then(() => {
-    el.adoptedStyleSheets.push(styles);
-  });
+	if (el) {
+		const styles = new CSSStyleSheet();
+		styles.replace(cssText).then(() => {
+			el.adoptedStyleSheets.push(styles);
+		});
+	}
 };
+
+const forEachShadowRoot = (
+	selector: string,
+	callback: (el: ShadowRoot) => void,
+) => {
+	for (const { shadowRoot } of document.querySelectorAll(selector)) {
+		if (shadowRoot) callback(shadowRoot);
+	}
+};
+
 const applyStylesForEach = (selector: string, styles: string) => {
-  for (const el of document.querySelectorAll(selector))
-    applyStyles(el.shadowRoot, styles);
+	forEachShadowRoot(selector, (shadowRoot) => {
+		applyStyles(shadowRoot, styles);
+	});
 };
+
 const getShadowRoot = (selector: string, parent: ParentNode = document) =>
-  parent.querySelector(selector)?.shadowRoot;
+	parent.querySelector(selector)?.shadowRoot;
 
 applyStyles(document, sGLOBAL);
 
 applyStyles(
-  getShadowRoot("menu-top-hamburger", getShadowRoot("menu-top")),
-  sHAMBURGER,
+	getShadowRoot("menu-top-hamburger", getShadowRoot("menu-top")),
+	sHAMBURGER,
 );
 
 applyStyles(getShadowRoot("usos-copyright"), sCOPYRIGHT);
 
-applyStylesForEach("usos-selector", sSELECTOR);
+forEachShadowRoot("usos-selector", (shadowRoot) => {
+	applyStyles(shadowRoot, sSELECTOR);
+	applyStyles(getShadowRoot("text-field", shadowRoot), sTEXT_FIELD);
+});
 
 applyStyles(getShadowRoot("usos-timetable"), sTIMETABLE);
 applyStylesForEach("timetable-day", sTIMETABLE_DAY);
@@ -36,19 +53,19 @@ applyStylesForEach("timetable-day", sTIMETABLE_DAY);
 type RgbString = `rgb(${number}, ${number}, ${number})`;
 
 const tableColors = {
-  "rgb(236, 236, 236)": "#555",
-  "rgb(216, 216, 216)": "#444",
-  "rgb(238, 238, 221)": "#444",
-  "rgb(222, 222, 205)": "#333",
-  "rgb(156, 164, 152)": "#222",
-  "rgb(255, 255, 255)": "#333",
+	"rgb(236, 236, 236)": "#555",
+	"rgb(216, 216, 216)": "#444",
+	"rgb(238, 238, 221)": "#444",
+	"rgb(222, 222, 205)": "#333",
+	"rgb(156, 164, 152)": "#222",
+	"rgb(255, 255, 255)": "#333",
 } satisfies Record<RgbString, string>;
 type TableColor = keyof typeof tableColors;
 
 setTimeout(() => {
-  for (const td of document.querySelectorAll<HTMLTableCellElement>(
-    "tbody > tr > td, tbody > tr > th",
-  ))
-    td.style.backgroundColor =
-      tableColors[td.style.backgroundColor as TableColor] || "#000";
+	for (const td of document.querySelectorAll<HTMLTableCellElement>(
+		"tbody > tr > td, tbody > tr > th",
+	))
+		td.style.backgroundColor =
+			tableColors[td.style.backgroundColor as TableColor] || "#000";
 }, 100);
